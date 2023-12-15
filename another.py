c1f72d5fb1f3088685053d251f4acd51b3013edb
vinimach02 <66345501+vinimach02@users.noreply.github.com>
2023-12-15 17:49:08 -0300

    token nothing

--- another.py
+++ another.py
@@ -1,33 +1,45 @@
 import requests
 
-# vCenter connection parameters
-vcenter_host = "your-vcenter-host"
-vcenter_user = "your-username"
-vcenter_password = "your-password"
+def get_disk_info(auth_token, vcenter_host, vm_id, disk_id):
+    capacity_url = f"https://{vcenter_host}/rest/vcenter/vm/{vm_id}/hardware/disk/{disk_id}/capacity"
+    used_space_url = f"https://{vcenter_host}/rest/vcenter/vm/{vm_id}/hardware/disk/{disk_id}"
 
-# VM and disk information
-vm_id = "your-vm-id"
-disk_id = "your-disk-id"
+    capacity_response = requests.get(capacity_url, headers={"vmware-api-session-id": auth_token}, verify=False)
+    used_space_response = requests.get(used_space_url, headers={"vmware-api-session-id": auth_token}, verify=False)
 
-# Authentication and obtaining the session token
-auth_url = f"https://{vcenter_host}/rest/com/vmware/cis/session"
-auth_response = requests.post(auth_url, auth=(vcenter_user, vcenter_password), verify=False)
-auth_token = auth_response.json()["value"]
+    capacity_response.raise_for_status()
+    used_space_response.raise_for_status()
 
-# Get capacity information for the specified disk
-capacity_url = f"https://{vcenter_host}/rest/vcenter/vm/{vm_id}/hardware/disk/{disk_id}/capacity"
-capacity_response = requests.get(capacity_url, headers={"vmware-api-session-id": auth_token}, verify=False)
-capacity = capacity_response.json()["value"]
+    capacity = capacity_response.json()["value"]
+    used_space = used_space_response.json()["value"]
 
-# Get used space information for the specified disk
-used_space_url = f"https://{vcenter_host}/rest/vcenter/vm/{vm_id}/hardware/disk/{disk_id}"
-used_space_response = requests.get(used_space_url, headers={"vmware-api-session-id": auth_token}, verify=False)
-used_space = used_space_response.json()["value"]
+    return capacity, used_space
 
-# Calculate free space by subtracting used space from capacity
-free_space = capacity - used_space
+def main():
+    # vCenter connection parameters
+    vcenter_host = "your-vcenter-host"
 
-# Print the results
-print(f"Capacity: {capacity} bytes")
-print(f"Used Space: {used_space} bytes")
-print(f"Free Space: {free_space} bytes")
+    # VM and disk information
+    vm_id = "your-vm-id"
+    disk_id = "your-disk-id"
+
+    # Replace 'your-auth-token' with the actual authentication token
+    auth_token = "your-auth-token"
+
+    try:
+        # Get disk information
+        capacity, used_space = get_disk_info(auth_token, vcenter_host, vm_id, disk_id)
+
+        # Calculate free space
+        free_space = capacity - used_space
+
+        # Print the results
+        print(f"Capacity: {capacity} bytes")
+        print(f"Used Space: {used_space} bytes")
+        print(f"Free Space: {free_space} bytes")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
